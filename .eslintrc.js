64d3148696c2e63ae02333688583e5f5cd0b4913
Jinksi <ericjinks@gmail.com>
2016-06-14 23:01:49 +1000

    Added eslint

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+    "env": {
+        "browser": true
+    },
+    "rules": {
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "never"
+        ]
+    }
+};
